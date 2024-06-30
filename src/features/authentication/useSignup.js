b6f5c7c1cf9fccc3763da5fbfdc5ea23033e7067
Tomo <tomobratic23@gmail.com>
2024-06-30 18:48:38 +0200

    set user to query client when signing up

--- src/features/authentication/useSignup.js
+++ src/features/authentication/useSignup.js
@@ -1,13 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { signUp } from "../../services/apiAuthentication";
 import toast from "react-hot-toast";
 
 export function useSignup() {
+  const queryClient = useQueryClient();
   const { mutate: signup, isLoading: isSigning } = useMutation({
     mutationFn: ({ signUpEmail, signUpPassword, userName, phone, avatar }) =>
       signUp({ signUpEmail, signUpPassword, userName, phone, avatar }),
-    onSuccess: () => {
+    onSuccess: (user) => {
       toast.success("Account successfully created!");
+      queryClient.setQueryData(["user"], user.user);
     },
     onError: (error) => {
       toast.error(`Something went wrong while signing up: ${error.message}`);
